4d3e0553036c38f067bebcb5fb6802069417326c
unknown <ctsjavauser298@iiht.tech>
2020-09-02 18:43:48 +0530

    sprint 2 added eureka client and port number changes

--- loan-search-service/src/main/java/com/scb/pod2/loansearch/loansearchservice/LoanSearchServiceApplication.java
+++ loan-search-service/src/main/java/com/scb/pod2/loansearch/loansearchservice/LoanSearchServiceApplication.java
@@ -2,8 +2,10 @@ package com.scb.pod2.loansearch.loansearchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LoanSearchServiceApplication {
 
 	public static void main(String[] args) {

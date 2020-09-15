eabafabec0176e552cf992040931ffa6cd524aff
unknown <ctsjavauser279@iiht.tech>
2020-09-15 21:03:24 +0530

    loan-search-service

--- loan-search-service/src/main/java/com/scb/pod2/loansearch/loansearchservice/controller/LoanSearchController.java
+++ loan-search-service/src/main/java/com/scb/pod2/loansearch/loansearchservice/controller/LoanSearchController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ import com.scb.pod2.loansearch.loansearchservice.service.LoanSearchService;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/loansearch")
 public class LoanSearchController {
 
 	private Logger logger = LoggerFactory.getLogger(LoanSearchController.class);

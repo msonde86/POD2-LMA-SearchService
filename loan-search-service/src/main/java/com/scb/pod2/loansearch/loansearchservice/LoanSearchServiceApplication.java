b6b6fb781736eeba860cdca7b45340b87595920f
ctsjavauser279 <ctsjavauser279@ctsjavafs70.iiht.tech>
2020-09-18 12:54:48 +0530

    "zuul filter commited"

--- loan-search-service/src/main/java/com/scb/pod2/loansearch/loansearchservice/LoanSearchServiceApplication.java
+++ loan-search-service/src/main/java/com/scb/pod2/loansearch/loansearchservice/LoanSearchServiceApplication.java
@@ -3,14 +3,12 @@ package com.scb.pod2.loansearch.loansearchservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 import com.scb.pod2.loansearch.loansearchservice.filter.ZuulPreFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class LoanSearchServiceApplication {
 
 	public static void main(String[] args) {
